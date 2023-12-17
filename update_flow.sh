eb0681b7a1ba2be30b328cd3147ef2f3d86779a6
Albert Maršík <albert.marsik@gmail.com>
2023-12-17 19:48:36 +0100

    cleanup

--- update_flow.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp convform/bots/vtipobot_edited.json ../convform/bots/vtipobot.json
-cp -r ../convform/bots/ convform/bots/
-cp ../convform/Cargo.toml convform/Cargo.toml
-
-# cd convform/ && maturin develop --features debug
-python test.py
