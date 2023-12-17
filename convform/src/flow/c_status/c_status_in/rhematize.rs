ed1f17ef11bb20061eebe204012544f0b2686439
Albert Maršík <albert.marsik@gmail.com>
2023-12-17 19:45:28 +0100

    cleanup

--- convform/src/flow/c_status/c_status_in/rhematize.rs
+++
@@ -1,6 +0,0 @@
-use super::match_states::MatchItem;
-
-pub fn rhematize(mut m: Vec<MatchItem>) -> Vec<&str> {
-    let _ = &mut m.sort_by_key(|ms| ms.get_index());
-    m.into_iter().flat_map(|m| m.get_states().to_vec()).collect()
-}
