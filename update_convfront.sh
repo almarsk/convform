29a6f9419637a0bade4a0f3099181f9b4c819213
Albert Maršík <albert.marsik@gmail.com>
2024-04-17 13:21:10 +0200

    setup

--- update_convfront.sh
+++ update_convfront.sh
@@ -1,5 +1,15 @@
 cd convfront
 npm run build
+
+if [ ! -d "assets" ]; then
+    mkdir assets
+fi
+
 cp -r dist/assets/* ../assets
+
+if [ ! -d "templates" ]; then
+    mkdir templates
+fi
+
 cp dist/index.html ../templates
 cd ..
