56ce0325275186b2d1425b3edbcf5d046e49e9f1
Albert Maršík <albert.marsik@gmail.com>
2024-04-18 11:49:22 +0200

    persona, user instructions and panel title

--- update_convfront.sh
+++ update_convfront.sh
@@ -1,18 +1,22 @@
+
+# make dirs for flask if they aren't there
+if [ ! -d "templates" ]; then
+    mkdir templates
+fi
 if [ ! -d "assets" ]; then
     mkdir assets
 fi
 
+# remove previous content
 rm assets/*.js
 rm assets/*.css
 
+# build current version of frontend
 cd convfront
 npm run build
 
+# expose bundle to flask
 cp -r dist/assets/* ../assets
-
-if [ ! -d "templates" ]; then
-    mkdir templates
-fi
-
 cp dist/index.html ../templates
+
 cd ..
