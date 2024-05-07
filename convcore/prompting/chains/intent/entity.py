3d48cc42f98e8ed8acb9fad9f3c2eac1a9bc7105
almarsk <albert.marsik@gmail.com>
2024-05-07 17:32:16 +0200

    anafora touchup

--- convcore/prompting/chains/intent/entity.py
+++ convcore/prompting/chains/intent/entity.py
@@ -13,7 +13,7 @@ def entity(args, bench=False):
     if bench:
         start_time = time.time()
 
-    chat = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0, max_tokens=800)
+    chat = ChatOpenAI(model="gpt-4-turbo-2024-04-09", temperature=0, max_tokens=800)
     messages: list[HumanMessage | AIMessage | SystemMessage] = list()
 
     messages.append(SystemMessage(content=f"""\
