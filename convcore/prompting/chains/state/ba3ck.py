e148c11376bef11b9a2100a50b3a0875d426aa74
almarsk <albert.marsik@gmail.com>
2024-05-07 19:24:02 +0200

    anafora touchup

--- convcore/prompting/chains/state/ba3ck.py
+++ convcore/prompting/chains/state/ba3ck.py
@@ -7,14 +7,16 @@ from convcore.prompting.chains.state.basic import basic
 
 import time
 
+how_far = 6
+
 def ba3ck(args, bench=False):
 
     used_context = []
     if "context" in args:
         context_len = len(args["context"])
-        if args["context"] and context_len > 4:
+        if args["context"] and context_len > how_far:
             args["log"]([f"going back"])
-            used_context = args["context"][0:-4]
+            used_context = args["context"][0:-how_far]
 
     args["context"] = used_context
 
