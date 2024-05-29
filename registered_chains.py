61f894d201651df2425bce8901fad8cbcaaaec05
almarsk <albert.marsik@gmail.com>
2024-05-29 11:05:42 +0200

    anafora 2

--- registered_chains.py
+++ registered_chains.py
@@ -1,4 +1,4 @@
 registered_chains = {
-    "state": ["basic", "anafora", "ba3ck", "b_dynamic"],
+    "state": ["basic", "anafora", "an2fora", "ba3ck", "b_dynamic"],
     "intent": ["basic", "entity", "enstity"]
 }
