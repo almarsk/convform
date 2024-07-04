3e738d63a8e83600934f69aaf3593b32fcd2829a
almarsk <albert.marsik@gmail.com>
2024-07-04 22:10:02 +0200

    debug grade

--- convfront/src/convo/Outro.jsx
+++ convfront/src/convo/Outro.jsx
@@ -41,7 +41,7 @@ const Outro = () => {
           placeholder="komentář"
         ></textarea>
         <div className="outro-eval">
-          <select name="grade" required className="submit" defaultValue="">
+          <select name="grade" required className="submit" defaultValue="3">
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
