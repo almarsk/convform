bcbb3fafec6d6faa14d8cef9a4b7917dc18a9f8b
almarsk <albert.marsik@gmail.com>
2025-02-26 11:16:45 +0100

    quantitative analysis written

--- playground_json.py
+++ playground_json.py
@@ -2,22 +2,8 @@ import json
 import pprint
 from functools import reduce
 
-with open("export/final_annotated_data.json", "r") as d:
+with open("notes/data/final_annotated_data.json", "r") as d:
     data = json.load(d)
 
-
-    print(f"Number of convos: {len(data)}")
-
-    zero_anaphora_number = len([convo for convo in data if 'zero_anaphora' in convo and convo['zero_anaphora']])
-    print(f"Number of zero anaphora conversations: {zero_anaphora_number}")
-
-
-    def get_rating_avg(stimulus_type: str):
-        _rated = [c["rating"] for c in data if "rating" in c and c["rating"] and "type" in c and stimulus_type in c["type"]]
-        _sum_score = sum([c for c in _rated])
-        rating_avg = _sum_score/len(_rated)
-        print(f"rating {stimulus_type} {rating_avg}")
-
-    get_rating_avg("shallow")
-    get_rating_avg("deep")
-    get_rating_avg("nonassignable")
+    print(sum([c["rating"] or 5 for c in data if "rating" in c])/len(data))
+    print(sum([c["rating"] for c in data if "rating" in c and c["rating"]])/len([c for c in data if "rating" in c and c["rating"]]))
